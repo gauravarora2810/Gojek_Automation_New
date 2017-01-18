ba1d2fba327138e8dece24dfaf35e2d0a933a302
gaurav.arora <gaurav.arora@sirionlabs.com>
2017-01-18 20:42:16 +0530

    Fixed the Assert error, removed Junit package and added TestNG

--- src/test/resources/com/sirion/suite/cr/ChangeRequestCreation.java
+++ src/test/resources/com/sirion/suite/cr/ChangeRequestCreation.java
@@ -1,16 +1,17 @@
 package test.resources.com.sirion.suite.cr;
 
-import junit.framework.Assert;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+
 import test.resources.com.sirion.util.*;
 
 public class ChangeRequestCreation extends TestSuiteBase
